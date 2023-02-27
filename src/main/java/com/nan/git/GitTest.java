3147186b0bba5c074d1e9c863fc5d21caffa5dff
nan <1440528931@qq.com>
2023-02-27 15:54:10 +0800

    second commit

--- src/main/java/com/nan/git/GitTest.java
+++ src/main/java/com/nan/git/GitTest.java
@@ -3,5 +3,6 @@ package com.nan.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
     }
 }
